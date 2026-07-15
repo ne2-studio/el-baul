3eaa9d35c85663ec0b46cfeb8ff1a0254dd7c08d
Pedro Pardal <ppardalj@gmail.com>
2026-07-15 19:39:46 +0200

    feat: add android app with capacitor

---
+++ app/android/app/src/main/java/studio/ne2/elbaul/MainActivity.java
@@ -0,0 +1,5 @@
+package studio.ne2.elbaul;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'studio.ne2.elbaul',
+  appName: 'El Baúl',
+  webDir: 'dist'
+};
+
+export default config;
